23487f42b63daff9547e1af85264c61963b14d48
IAmGurich <sashhurk@gmail.com>
2026-05-31 21:01:03 +0300

    Deleted comments

--- src/main/java/com/lavaboatclutch/util/LbcBoatImmunity.java
+++ src/main/java/com/lavaboatclutch/util/LbcBoatImmunity.java
@@ -5,4 +5,7 @@ public interface LbcBoatImmunity {
     void lbc_setImmunityTicks(int ticks);
     boolean lbc_wasInLavaLastTick();
     void lbc_setWasInLavaLastTick(boolean value);
+
+    int  lbc_getFireSuppressTicks();
+    void lbc_setFireSuppressTicks(int ticks);
 }
