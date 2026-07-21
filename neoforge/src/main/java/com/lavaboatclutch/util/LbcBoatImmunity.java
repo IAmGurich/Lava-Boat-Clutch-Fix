7e9a73c64ec555824dc8b06f05de28f5b5d63616
IAmGurich <sashhurk@gmail.com>
2026-07-21 21:37:32 +0300

    Add NeoForge support

---
+++ neoforge/src/main/java/com/lavaboatclutch/util/LbcBoatImmunity.java
@@ -0,0 +1,15 @@
+package com.lavaboatclutch.util;
+
+public interface LbcBoatImmunity {
+    int  lbc_getImmunityTicks();
+    void lbc_setImmunityTicks(int ticks);
+    boolean lbc_wasInLavaLastTick();
+    void lbc_setWasInLavaLastTick(boolean value);
+
+    int  lbc_getFireSuppressTicks();
+    void lbc_setFireSuppressTicks(int ticks);
+
+    
+    boolean lbc_isImmunityGranted();
+    void    lbc_setImmunityGranted(boolean value);
+}
